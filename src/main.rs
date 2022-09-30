db0896d24e90b9befedfad9a972dbef229ba429e
ravi maithrey <maitrey.ind@gmail.com>
2022-09-30 15:15:33 -0400

    from my pc

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use std::usize;
-
+use std::f64::{sqrt, powf};
 use minifb::{ScaleMode, Window, WindowOptions};
 
 const WIDTH: usize = 1280 / 2;
 const HEIGHT: usize = 640 / 2;
+
+struct Body {
+    mass: u64,
+    x: i64,
+    y: i64,
+}
+
+struct Force {
+    magnitude: u64,
+    x: i64,
+    y: i64,
+}
+
+fn gravitational_force(body1: Body, body2: Body) -> Force {
+    Force {
+        magnitude: body1.mass * body2.mass
+            / sqrt((body1.x - body2.x).powf(2.0) + (body1.y - body2.y).powf(2.0)),
+        x: (body1.x - body2.x).max(body2.x - body1.x),
+        y: (body1.y - body2.y).max(body2.y - body1.y),
+    }
+}
 
 fn main() {
     let mut window = Window::new(
@@ -19,8 +39,8 @@ fn main() {
     .expect("Unable to create window");
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    // let mut buffer : Vec<u32> = vec![0; 1280 * 640];
-   loop {
+    // let mut buffer : Vec<u64> = vec![0; 1280 * 640];
+    loop {
         window.update();
     }
 }
