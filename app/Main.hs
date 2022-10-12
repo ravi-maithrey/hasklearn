8b9e7c8be2b77ddaab6899d52d34c3ebc5be3c73
ravi maithrey <maitrey.ind@gmail.com>
2022-10-12 17:11:26 -0400

    from my pc

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 -- how do i genericize a container? instead of list, it can be any container
 
 dot :: [Float] -> [Float] -> Float
-dot (x : xs) (y : ys) = (x * y) : dot xs ys
+dot xs ys = sum $ pure (*) <*> xs <*> ys
 
 hypothesis :: [Float] -> [Float] -> Float -> [Float]
-hypothesis xs' w b = pure (*) <*> w <*> xs' + b
+hypothesis xs' w b = fmap (+ b) (pure (*) <*> w <*> xs')
 
 cost :: [Float] -> [Float] -> Float
 cost ys hs = sum $ (** 2) <$> (pure (-) <*> ys <*> hs)
@@ -15,10 +15,10 @@ cost ys hs = sum $ (** 2) <$> (pure (-) <*> ys <*> hs)
 updateWeights :: [Float] -> [Float] -> [Float] -> [Float] -> Float -> [Float]
 updateWeights xs hs ys w lr = fmap (-(lr * dw)) w
   where
-    dw = (2 * dot xs (pure (-) <*> ys <*> hs)) / length xs -- number of training examples. might to change it up in a 2d array?
+    dw = (2 * dot xs (pure (-) <*> ys <*> hs)) / fromIntegral (length xs) -- number of training examples. might to change it up in a 2d array?
 
 -- dW = - ( 2 * ( self.X.T ).dot( self.Y - Y_pred )  ) / self.m
-updateBias :: [Float] -> [Float] -> [Float] -> Float -> Float -> [Float]
-updateBias xs hs ys b lr = fmap (-(lr * db)) b
+updateBias :: [Float] -> [Float] -> [Float] -> Float -> Float -> Float
+updateBias xs hs ys b lr = b - (lr * db)
   where
-    db = 2 * sum (pure (-) <*> ys <*> hs) / length xs
+    db = 2 * sum (pure (-) <*> ys <*> hs) / fromIntegral length xs
