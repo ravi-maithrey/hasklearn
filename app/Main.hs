8d6e08b40d0443af113e22a2a6848c8338097b50
ravi maithrey <maitrey.ind@gmail.com>
2022-11-04 10:08:46 -0400

    from my pc

--- app/Main.hs
+++ app/Main.hs
@@ -8,22 +8,52 @@ import GHC.Float (int2Float)
 dot :: [Float] -> [Float] -> Float
 dot xs ys = sum $ zipWith (*) xs ys
 
-predict :: [Float] -> [Float] -> Float -> [Float]
-predict xs w b = fmap (+ b) (zipWith (*) xs w)
+-- predict :: [Float] -> [Float] -> Float -> [Float]
+-- predict xs w b = fmap (+ b) (zipWith (*) xs w)
 
-cost :: [Float] -> [Float] -> Float
-cost ys hs = sum $ (** 2) <$> zipWith (-) ys hs
+-- cost :: [Float] -> [Float] -> Float
+-- cost ys hs = sum $ (** 2) <$> zipWith (-) ys hs
 
-predict' model xts = model xts
+-- predict' model xts = model xts
 
-linModel = linearRegression xtrs ytrs
+-- linModel = linearRegression xtrs ytrs
 
--- fittedModel = fit linModel
+-- -- fittedModel = fit linModel
 
-linearRegression :: [Float] -> [Float] -> ([Float], Float)
-linearRegression xtrs ytrs = (execState (updateWeights xtrs ytrs) (replicate (length xtrs) (0 :: Float)), execState (updateBias xtrs ytrs) (0::Float))
+-- linearRegression :: [Float] -> [Float] -> ([Float], Float)
+-- linearRegression xtrs ytrs = (execState (updateWeights xtrs ytrs) (replicate (length xtrs) (0 :: Float)), execState (updateBias xtrs ytrs) (0::Float))
 
-updateWeights xtrs ytrs = do
-  weights <- get
-  let dw = 
--- linearRegression xtrs ytrs = 
+-- updateWeights xtrs ytrs = do
+--   weights <- get
+--   let dw =
+-- linearRegression xtrs ytrs =
+
+data Model a = Model {weights :: [a], bias :: a} deriving (Eq, Show)
+
+instance Functor Model where
+  fmap f model = model {weights = fmap f (weights model), bias = f (bias model)}
+
+initialize :: [Float] -> [Float] -> Model Float
+initialize xs ys = Model {weights = replicate (length xs) 0, bias = 0}
+
+xs :: [Float]
+xs = [1, 2, 3, 4]
+
+ys :: [Float]
+ys = [1, 2, 3, 4]
+
+linearReg :: Model Float
+linearReg = initialize xs ys
+
+predict :: [Float] -> Model Float -> [Float]
+predict xsTe model = fmap (+ b) (zipWith (*) xs w)
+  where
+    w = weights model
+    b = bias model
+
+fit :: Model Float -> [Float] -> [Float] -> Model Float
+fit model xTrs yTrs = model {weights = updateWeights (weights model) xTrs yTrs, bias = updateBias (bias model) xTrs yTrs}
+
+updateWeights = undefined
+
+updateBias = undefined
