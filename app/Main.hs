e5e6c59441ebee7b0cf7cd15c38b89f79df341fc
ravi maithrey <maitrey.ind@gmail.com>
2022-11-04 14:04:30 -0400

    from my pc

--- app/Main.hs
+++ app/Main.hs
@@ -42,8 +42,8 @@ xs = [1, 2, 3, 4]
 ys :: [Float]
 ys = [1, 2, 3, 4]
 
-linearReg :: Model Float
-linearReg = initialize xs ys
+linearReg :: [Float] -> [Float] -> Model Float
+linearReg = initialize
 
 predict :: [Float] -> Model Float -> [Float]
 predict xsTe model = fmap (+ b) (zipWith (*) xs w)
@@ -53,7 +53,22 @@ predict xsTe model = fmap (+ b) (zipWith (*) xs w)
 
 fit :: Model Float -> [Float] -> [Float] -> Model Float
 fit model xTrs yTrs = model {weights = updateWeights (weights model) xTrs yTrs, bias = updateBias (bias model) xTrs yTrs}
+  where
+    updateWeights weight xTr yTr = execState (modifyWeight xTr yTr model) weight
+    updateBias bias xTr yTr = execState (modifyBias xTr yTr model) bias
 
-updateWeights = undefined
+modifyWeight xTr yTr model = do
+  weight <- get
+  let dw = -(2 * dot xTr (zipWith (-) yTr (predict xTr model))) / fromIntegral (length xTr)
+  put (weight - (0.01 * dw))
 
-updateBias = undefined
+modifyBias xTr yTr model = do
+  bias <- get
+  let db = -2 * sum (zipWith (-) yTr (predict xTr model)) / fromIntegral (length xTr)
+  put (bias - (0.01 * db))
+
+-- updateWeights weight xTr yTr = execState (State s a) s
+-- updateBias biases xTr yTr = undefined
+
+cost :: Model Float -> [Float] -> [Float] -> Float
+cost model xs ys = sum (fmap (^ 2) (zipWith (-) ys (predict xs model))) / fromIntegral (length xs)
