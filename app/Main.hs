aa8fc93a628e486b2f36edc2550c9d1d3ba85456
ravi maithrey <maitrey.ind@gmail.com>
2022-11-10 10:57:21 -0500

    from my pc

--- app/Main.hs
+++ app/Main.hs
@@ -8,26 +8,6 @@ import GHC.Float (int2Float)
 dot :: [Float] -> [Float] -> Float
 dot xs ys = sum $ zipWith (*) xs ys
 
--- predict :: [Float] -> [Float] -> Float -> [Float]
--- predict xs w b = fmap (+ b) (zipWith (*) xs w)
-
--- cost :: [Float] -> [Float] -> Float
--- cost ys hs = sum $ (** 2) <$> zipWith (-) ys hs
-
--- predict' model xts = model xts
-
--- linModel = linearRegression xtrs ytrs
-
--- -- fittedModel = fit linModel
-
--- linearRegression :: [Float] -> [Float] -> ([Float], Float)
--- linearRegression xtrs ytrs = (execState (updateWeights xtrs ytrs) (replicate (length xtrs) (0 :: Float)), execState (updateBias xtrs ytrs) (0::Float))
-
--- updateWeights xtrs ytrs = do
---   weights <- get
---   let dw =
--- linearRegression xtrs ytrs =
-
 data Model a = Model {weights :: [a], bias :: a} deriving (Eq, Show)
 
 instance Functor Model where
