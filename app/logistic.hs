bac0063d6a4fe4cdb7e510c674737f1d4866583b
ravi maithrey <maitrey.ind@gmail.com>
2022-11-27 11:26:14 -0500

    from my pc

--- app/logistic.hs
+++ app/logistic.hs
@@ -1,3 +1,10 @@
 module Logistic where
 
-import Main
+import Model
+
+sigmoid :: Float -> Float
+sigmoid x = 1 / (1 + exp (-x))
+
+predictLogit :: Model Float -> [Float] -> [Float]
+
+preditLogit model xsTe = fmap sigmoid (dot xsTe w + b)
