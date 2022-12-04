6cbd069cb78d2ff8ce579c9ed44bb57c8433f3dd
ravi maithrey <maitrey.ind@gmail.com>
2022-12-03 21:16:14 -0500

    from my pc

--- app/naive.hs
+++ app/naive.hs
@@ -2,5 +2,11 @@ module Naive where
 
 data Data a = Data {input :: [a], output :: a}
 
-instance Functor Data a where
-  fmap f data = data {input = fmap f (input data), output = output data}
+-- instance Functor Data where
+--   fmap f datas = datas {input = fmap f (input datas), output = output datas}
+
+-- this is the smoothing factor
+k = 1
+
+countClasses :: [Data a] -> (Integer, Integer)
+countClasses datas = (fmap (\a -> if a == 0 then 1 else 0) (output <$> datas), fmap (\a -> if a == 1 then 1 else 0) (output <$> datas))
